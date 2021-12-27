da3deeb2f8745f40750f7fcf7eb166d01782f9c3
Vladislav <50212442+vsnedro@users.noreply.github.com>
2021-12-27 21:49:57 +0300

    Added SimpleAutoMapper example.

---
+++ SimpleAutomapper/SimpleAutoMapper.Example/Classes.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleAutoMapper.Example;
+
+internal class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id = {Id}; Name = {Name}; Email = {Email};";
+    }
+}
+
+internal class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id = {Id}; Name = {Name}; Email = {Email};";
+    }
+}
---
+++ SimpleAutomapper/SimpleAutoMapper.Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Running;
+
+using SimpleAutoMapper.Example;
+using SimpleAutoMapper.Benchmarks;
+using SimpleAutoMapper;
+
+var mapper = new SimpleMapper<Person, User>();
+var alexPerson = new Person() { Id = 1, Name = "Alex", Email = "alex@local" };
+Console.WriteLine($"Alex as person: {alexPerson}");
+var alexUser = mapper.Map(alexPerson);
+Console.WriteLine($"Alex as user: {alexUser}");
+
+BenchmarkRunner.Run<SimpleMapperBenchmark>();
+
+Console.WriteLine("\nPress any key to exit.");
+Console.ReadKey();
